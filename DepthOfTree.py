f2623c1dc49134a3e4cc06d05c9da0758810bfe0
Minh Nguyen <44143370+minhmo1620@users.noreply.github.com>
2019-12-09 14:07:24 +0900

    Create DepthOfTree.py

---
+++ DepthOfTree.py
@@ -0,0 +1,22 @@
+#link: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+#solution approach: recursion
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        if root.left == None and root.right == None:
+            return 1
+        elif root.left == None:
+            return 1 + self.minDepth(root.right)
+        elif root.right == None:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+        
